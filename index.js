37644c8c703844ac8818f3556245cbe54748ee21
Dawud-Shuaibu <dawudshuaibu6565@gmail.com>
2024-10-23 14:11:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+let home = document.getElementById('home');
+let about = document.getElementById('about');
+let projects = document.getElementById('projects');
+let contact = document.getElementById('contact');
+let home_content = document.getElementById('home-content');
+let about_content = document.getElementById('about-content');
+let projects_content = document.getElementById('projects-content');
+let contact_content = document.getElementById('contact-content')
+
+// Home Navigation Style
+home.style.backgroundColor = '#fff';
+home.style.paddingLeft = '3vw';
+home.style.paddingRight = '3vw';
+home.style.paddingBottom = '1vh';
+home.style.paddingTop = '1vh';
+home.style.borderTopLeftRadius = '10px';
+home.style.borderTopRightRadius = '10px';
+home.style.borderBottomLeftRadius = '10px';
+home.style.borderBottomRightRadius = '10px';
+
+// About Navigation Style
+about.style.backgroundColor = '#fff';
+about.style.paddingLeft = '3vw';
+about.style.paddingRight = '3vw';
+about.style.paddingBottom = '1vh';
+about.style.paddingTop = '1vh';
+about.style.borderTopLeftRadius = '10px';
+about.style.borderTopRightRadius = '10px';
+about.style.borderBottomLeftRadius = '10px';
+about.style.borderBottomRightRadius = '10px';
+
+
+// Projects Navigation Style
+projects.style.backgroundColor = '#fff';
+projects.style.paddingLeft = '3vw';
+projects.style.paddingRight = '3vw';
+projects.style.paddingBottom = '1vh';
+projects.style.paddingTop = '1vh';
+projects.style.borderTopLeftRadius = '10px';
+projects.style.borderTopRightRadius = '10px';
+projects.style.borderBottomLeftRadius = '10px';
+projects.style.borderBottomRightRadius = '10px';
+
+// Contact  Navigation Style
+contact.style.backgroundColor = '#fff';
+contact.style.paddingLeft = '3vw';
+contact.style.paddingRight = '3vw';
+contact.style.paddingBottom = '1vh';
+contact.style.paddingTop = '1vh';
+contact.style.borderTopLeftRadius = '10px';
+contact.style.borderTopRightRadius = '10px';
+contact.style.borderBottomLeftRadius = '10px';
+contact.style.borderBottomRightRadius = '10px';
+
+// "onClick" Navigation Style
+function home_nav() {
+    home.style.backgroundColor = '#fff';
+    home.style.paddingLeft = '3vw';
+    home.style.paddingRight = '3vw';
+    home.style.paddingBottom = '2vh';
+    home.style.paddingTop = '1vh';
+    home.style.borderTopLeftRadius = '10px';
+    home.style.borderTopRightRadius = '10px';
+    home.style.borderBottomLeftRadius = '0px';
+    home.style.borderBottomRightRadius = '0px';
+}
+
+function about_nav() {
+    about.style.backgroundColor = '#fff';
+    about.style.paddingLeft = '3vw';
+    about.style.paddingRight = '3vw';
+    about.style.paddingBottom = '2vh';
+    about.style.paddingTop = '1vh';
+    about.style.borderTopLeftRadius = '10px';
+    about.style.borderTopRightRadius = '10px';
+    about.style.borderBottomLeftRadius = '0px';
+    about.style.borderBottomRightRadius = '0px';
+}
+
+function projects_nav() {
+    projects.style.backgroundColor = '#fff';
+    projects.style.paddingLeft = '3vw';
+    projects.style.paddingRight = '3vw';
+    projects.style.paddingBottom = '2vh';
+    projects.style.paddingTop = '1vh';
+    projects.style.borderTopLeftRadius = '10px';
+    projects.style.borderTopRightRadius = '10px';
+    projects.style.borderBottomLeftRadius = '0px';
+    projects.style.borderBottomRightRadius = '0px';
+}
+
+function contact_nav() {
+    contact.style.backgroundColor = '#fff';
+    contact.style.paddingLeft = '3vw';
+    contact.style.paddingRight = '3vw';
+    contact.style.paddingBottom = '2vh';
+    contact.style.paddingTop = '1vh';
+    contact.style.borderTopLeftRadius = '10px';
+    contact.style.borderTopRightRadius = '10px';
+    contact.style.borderBottomLeftRadius = '0px';
+    contact.style.borderBottomRightRadius = '0px';
+}
+
+home.onclick = home_nav;
+contact.onclick = contact_nav;
